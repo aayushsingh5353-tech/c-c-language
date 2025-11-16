629ef63413b96fd563d693c8be644ea68af63484
aayushsingh5353-tech <aayushsingh5353@gmail.com>
2025-11-16 16:58:08 +0530

    basic 2nd code

---
+++ operators.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+using namespace std;
+
+int main()  {
+    int a = 100, b = 99;
+    int sum = a + b;
+
+    cout << "sum ="  << (a+b) << endl;
+    cout << "difference =" << (a-b) << endl;
+    cout << "product =" << (a*b) << endl;
+    cout << "division=" << (a/b) << endl;
+    cout << "modulo=" << (a%b) << endl;
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main()  {
+    int a = 99, b = 99;
+
+    cout << (10/4) << endl;
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main()  {
+    int a = 100; 
+    double b = 99;
+
+    cout << (a/b) << endl;
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main()  {
+    int ans =(5/(double)2); 
+    cout << ans << endl;
+
+    return 0;
+    }
+
+#include <iostream>
+using namespace std;
+
+int main()  {
+    
+    cout << (3<5)<< endl;
+    cout << (3>5)<< endl;
+    cout << (3<=5)<< endl;
+    cout << (3>=5)<< endl;
+    cout << (3==5)<< endl;
+    cout << (3==3)<< endl;
+    cout << (3==3)<< endl;
+    cout<< (3!=3)<< endl;
+    cout<< (3!=5)<< endl;
+    return 0;}
+    
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << ( (3<1) || (3<1) ) << endl;
+    return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << ( (3>1) && (3>4) ) << endl;
+    return 0;
+
+
+}
+
+#include <iostream>
+using namespace std;
+
+int main()  {
+    int a, b;
+    cout << "enter a : ";
+    cin >> a;
+
+    cout << "enter b : ";
+    cin >> b;
+
+    int sum = a + b;
+    cout <<"sum = " << sum << endl;
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10;
+
+int b =a++; 
+cout <<"b = " << b << endl;
+cout <<"a = " << a << endl;
+
+ }
+
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10;
+
+int b =++a; 
+cout <<"b = " << b << endl;
+cout <<"a = " << a << endl;
+
+ }
+
+
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10;
+
+int b =a--; 
+cout <<"b = " << b << endl;
+cout <<"a = " << a << endl;
+
+ }
+
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10;
+
+int b =--a; 
+cout <<"b = " << b << endl;
+cout <<"a = " << a << endl;
+
+} 
+//###
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    char grade ='A';
+    cout << grade << endl;
+    
+    return 0; 
+}
+
